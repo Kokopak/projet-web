fb6fbde805c2814d7384bcae5d8031ec017e1f45
Chataignon Corentin <corentin.chataignon44@gmail.com>
2018-03-10 18:45:25 +0100

    :sparkles: Ajout fonction pour CA

--- src/main/java/fr/isis/cdg/model/DAO.java
+++ src/main/java/fr/isis/cdg/model/DAO.java
@@ -6,6 +6,8 @@
 package fr.isis.cdg.model;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -43,6 +45,37 @@ public class DAO {
         }
 
         return result;
+    }
+    
+    public float turnoverByCategory(String category, String dateDep, String dateArr) {
+        String sql = "select * "
+                + "from product " 
+                + "inner join purchase_order using (product_id) "
+                + "inner join product_code on (product_code = prod_code) "
+                + "inner join discount_code using(discount_code) "
+                + "where product_code=? "
+                + "and sales_date between ? and ?";
+        
+        float somme = 0;
+        
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, category);
+            stmt.setDate(2, Date.valueOf(dateDep));
+            stmt.setDate(3, Date.valueOf(dateArr));
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()) {
+                float prix = (rs.getFloat("purchase_cost") - (rs.getFloat("purchase_cost") * rs.getFloat("rate") / 100)) * rs.getInt("quantity") + rs.getFloat("shipping_cost");
+                somme += prix;
+//System.out.println(rs.getString("product_code")+ " "+ rs.getInt("quantity") + " " + rs.getDate("sales_date"));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return somme;
     }
 
 }
